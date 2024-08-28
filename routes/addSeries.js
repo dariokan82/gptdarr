fc76a84c4b1ce85a67dc1dc53a7c3f7159ee01b1
dariokan82 <dario.cannizzaro@gmail.com>
2024-08-28 10:25:31 +0100

    Update addSeries.js
    added overseer

--- routes/addSeries.js
+++ routes/addSeries.js
@@ -1,22 +1,28 @@
 import express from 'express';
 import sonarr from '../modules/sonarr.js';
+import overseerr from '../modules/overseerr.js'; // Import the Overseerr module
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
     const series = req.body.items;
+    const service = req.query.service || 'sonarr'; // Default to Sonarr if no service is specified
 
     if (!series || series.length === 0) {
         return res.status(400).json({ message: 'Invalid request' });
     }
 
-    sonarr.bulkAdd(series)
-        .then((response) => {
-            return res.status(200).json(response);
-        })
-        .catch((error) => {
-            return res.status(500).json(error);
-        });
+    try {
+        let response;
+        if (service === 'overseerr') {
+            response = await overseerr.bulkAdd(series);
+        } else {
+            response = await sonarr.bulkAdd(series);
+        }
+        return res.status(200).json(response);
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
 });
 
 export default router;
